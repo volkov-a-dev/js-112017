f8a1f5717af936542bdaa76f62f5305989d99c8d
a.volkov <a.volkov@richbrain.net>
2017-12-15 02:22:34 +0300

    fixed tasks 7.2

--- src/cookie.js
+++ src/cookie.js
@@ -6,7 +6,7 @@
  * - значение
  * - удалить (при нажатии на кнопку, выбранная cookie удаляется из браузера и таблицы)
  *
- * На странице должна быть форма для добавления новой cookie:
+ * На странице должна быть форма для добавления новой cookie:x`
  * - имя
  * - значение
  * - добавить (при нажатии на кнопку, в браузер и таблицу добавляется новая cookie с указанным именем и значением)
@@ -40,14 +40,35 @@ let addButton = homeworkContainer.querySelector('#add-button');
 let listTable = homeworkContainer.querySelector('#list-table tbody');
 
 filterNameInput.addEventListener('keyup', function() {
+console.info(filterNameInput.value)
 
+    let array = listCookes();
+    let searchText = filterNameInput.value;
+    let regex = new RegExp(searchText.split(' ').join('|'));
+
+    listTable.innerHTML = '';
+
+    for (let prop in array) {
+        let tr = document.createElement('tr');
+        let name = prop;
+        let val = array[prop];
+
+        if ((name + ' ' + val).match(regex)) {
+            tr.innerHTML = `<td>${name}</td><td>${val}</td><td><button id=${val}>Удалить</button></td>`;
+            listTable.appendChild(tr)
+        }
+
+    }
 });
 
+
+
 addButton.addEventListener('click', () => {
+    listTable.innerHTML = '';
     let name = addNameInput.value;
     let val = addValueInput.value;
-    let cookieVal = name + '=' + val;
-
+    let cookieVal = `${name}=${val}`;
+    // console.log(cookieVal);
     document.cookie = cookieVal;
 
     viewCookes();
@@ -58,23 +79,24 @@ function listCookes() {
     let a = document.cookie.split(';').reduce((prev, current) => {
         const [name, value] = current.split('=');
 
-        prev[name.trim()] = value.trim();
+        prev[name] = value;
 
         return prev
     }, {});
-console.log('===_____________________________',a)
+
     return a;
 }
 
 function viewCookes() {
     let getLists = listCookes();
 
-    // console.log('getLists', getLists)
-
     for (let prop in getLists) {
+        let tr = document.createElement('tr');
         let name = prop;
         let val = getLists[prop];
-        listTable.innerHTML = '';
+
+        tr.innerHTML = `<td>${name}</td><td>${val}</td><td><button id=${val}>Удалить</button></td>`;
+        listTable.appendChild(tr)
     }
 }
 
